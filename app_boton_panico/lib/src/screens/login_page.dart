8ccdc74dab9c6f6599075885ece1c77d2a8d22e3
Pablo <84303588+Puetate@users.noreply.github.com>
2022-12-21 07:26:26 -0500

    TREEJS-15 Creacion proyecto movil

--- app_boton_panico/lib/src/screens/login_page.dart
+++ app_boton_panico/lib/src/screens/login_page.dart
@@ -41,7 +41,187 @@ class LoginPageState extends State<LoginPage> {
 
     // ignore: prefer_const_constructors
     return Scaffold(
+      body: Stack(children: [
+        Container(
+          width: double.infinity,
+          //padding: const EdgeInsets.symmetric(vertical: 60),
+          padding: const EdgeInsets.only(top: 60),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromRGBO(0, 150, 136, 1),
+              Color.fromRGBO(56, 56, 76, 1),
+            ]),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Image.asset(
+                "assets/image/fast_alert_logo.png",
+                height: 170,
+              ),
+            ),
+          ],
+        ),
+        Transform.translate(
+          offset: const Offset(0, -30),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Card(
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                margin: const EdgeInsets.only(
+                    left: 20, right: 20, top: 260, bottom: 20),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(top: 15, bottom: 15),
+                        child: Text(
+                          "Bienvenido",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                      ),
+                      Form(
+                        key: formKey,
+                        child: Column(children: [
+                          TextFormField(
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(30),
+                            ],
+                            textInputAction: TextInputAction.next,
+                            keyboardType: TextInputType.emailAddress,
+                            controller: email,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.email),
+                              label: Text("Email"),
+                            ),
+                            onSaved: (value) => {emailValue = value},
+                            validator: (value) {
+                              if (value.isEmpty || value == null) {
+                                return "Ingrese su correo electronico";
+                              }
+                              return null;
+                            },
+                          ),
+                          TextFormField(inputFormatters: [
+                              LengthLimitingTextInputFormatter(15),
+                            ],
+                            textInputAction: TextInputAction.done,
+                            obscureText: true,
+                            controller: password,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.lock_person),
+                              label: Text("Contraseña"),
+                            ),
+                            onSaved: (value) => {passwordValue = value},
+                            validator: (value) {
+                              if (value.isEmpty || value == null) {
+                                return "Ingrese su contraseña";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 30),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 15),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(textButtonSesion),
+                                  if (_loading)
+                                    Container(
+                                      width: 20,
+                                      height: 20,
+                                      margin: const EdgeInsets.only(
+                                        left: 20,
+                                      ),
+                                      child: const CircularProgressIndicator(
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                ],
+                              ),
+                              onPressed: () {
+                                emailValue = email.text;
+                                passwordValue = password.text;
 
+                                _showHomePage(
+                                    context, emailValue, passwordValue);
+                              }),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              const Text("Guardar Sesión"),
+                              const Padding(
+                                  padding: EdgeInsets.only(right: 15)),
+                              Switch(
+                                  value: isSwitched,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      isSwitched = value;
+                                    });
+                                  }),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text("¿No estas registrado?"),
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                    textStyle: TextStyle(
+                                        color: Theme.of(context).primaryColor),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushNamed("/register");
+                                  },
+                                  child: const Text(
+                                    "Registrate",
+                                    style: TextStyle(color: Colors.blue),
+                                  ))
+                            ],
+                          )
+                        ]),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        if (_isNotConncet)
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration:
+                const BoxDecoration(color: Color.fromRGBO(56, 56, 76, 1)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ),
+      ]),
     );
   }
 
